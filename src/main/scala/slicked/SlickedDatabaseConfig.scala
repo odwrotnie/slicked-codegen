5dd07e4db1e38d15e3826939f7d27f025352cded
rzepaw <odwrotnie@gmail.com>
2017-03-20 22:58:48 +0100

    fxy

--- src/main/scala/slicked/SlickedDatabaseConfig.scala
+++ src/main/scala/slicked/SlickedDatabaseConfig.scala
@@ -2,7 +2,7 @@ package slicked
 
 import rzepaw.configuration.Configuration
 import slick.basic.DatabaseConfig
-import slick.jdbc.JdbcProfile
+import slick.jdbc.{H2Profile, JdbcProfile}
 
 object SlickedDatabaseConfig
   extends Configuration {
@@ -20,4 +20,9 @@ trait SlickedDatabaseConfig
   lazy val profile: JdbcProfile = SlickedDatabaseConfig.profile
   lazy val dbConfig: DatabaseConfig[JdbcProfile] = SlickedDatabaseConfig.dbConfig
   lazy val db: profile.api.Database = SlickedDatabaseConfig.dbConfig.db
+
+  if(profile.isInstanceOf[H2Profile]) {
+    // URL: jdbc:h2:tcp://localhost/mem:db
+    org.h2.tools.Server.createTcpServer("-tcpAllowOthers").start()
+  }
 }
