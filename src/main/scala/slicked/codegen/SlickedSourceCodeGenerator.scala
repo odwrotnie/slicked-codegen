a5788ae0a30ed3f673a39dfff4e68e11cae2c46f
rzepaw <odwrotnie@gmail.com>
2017-08-25 23:46:06 +0200

    EVO

--- src/main/scala/slicked/codegen/SlickedSourceCodeGenerator.scala
+++ src/main/scala/slicked/codegen/SlickedSourceCodeGenerator.scala
@@ -4,22 +4,47 @@ import slick.codegen.SourceCodeGenerator
 import slick.model.Model
 
 class SlickedSourceCodeGenerator(model: Model)
-  extends SourceCodeGenerator(model) {
+    extends SourceCodeGenerator(model) {
 
   // add custom import for added data types
   // override def code = "import my.package.Java8DateTypes._" + "\n" + super.code
 
-  val imports = "slicked.SlickedRow" ::
-    "slicked.SlickedTable" ::
-    "slicked.SlickMappers._" :: Nil
+  override def parentType = Some("slicked.SlickMappers")
 
-  override def code = imports.map(i => s"import $i").mkString("\n// Custom imports start\n", "\n", "\n// Custom imports end\n\n") + super.code
+  override def packageCode(profile: String,
+                           pkg: String,
+                           container: String,
+                           parentType: Option[String]): String = {
+    s"""package ${pkg}
+       |
+       |trait ${container}${parentType.map(t => s" extends $t").getOrElse("")} {
+       |
+       |  self: slicked.HasDatabaseProfile =>
+       |
+       |  val profile: slick.jdbc.JdbcProfile
+       |  import profile.api._
+       |
+       |  ${indent(code)}
+       |}
+      """.stripMargin
+  }
+
+  val imports = "slicked.SlickedRow" ::
+    "slicked.SlickedTable" :: Nil
+
+  override def code =
+    imports
+      .map(i => s"import $i")
+      .mkString("\n// Custom imports start\n",
+                "\n",
+                "\n// Custom imports end\n\n") + super.code
 
   // Customize Scala entity name (case class, etc.)
   override def entityName = (dbTableName: String) => dbTableName.toCamelCase
 
   // Customize Scala table name (table class, table values, ...)
-  override def tableName = (dbTableName: String) => dbTableName.toCamelCase + "Table"
+  override def tableName =
+    (dbTableName: String) => dbTableName.toCamelCase + "Table"
 
   override def Table = new Table(_) {
     table =>
@@ -27,42 +52,54 @@ class SlickedSourceCodeGenerator(model: Model)
     override def Column = new Column(_) {
       override def rawType: String = model.tpe match {
         case "java.sql.Timestamp" => "org.joda.time.DateTime"
-        case _ => super.rawType
+        case _                    => super.rawType
       }
     }
 
     // Use different factory and extractor functions for tables with > 22 columns
-    override def factory   = if(columns.size == 1) TableClass.elementType else if(columns.size <= 22) s"${TableClass.elementType}.tupled" else s"${EntityType.name}.apply"
-    override def extractor = if(columns.size <= 22) s"${TableClass.elementType}.unapply" else s"${EntityType.name}.unapply"
+    override def factory =
+      if (columns.size == 1) TableClass.elementType
+      else if (columns.size <= 22) s"${TableClass.elementType}.tupled"
+      else s"${EntityType.name}.apply"
+    override def extractor =
+      if (columns.size <= 22) s"${TableClass.elementType}.unapply"
+      else s"${EntityType.name}.unapply"
 
     override def EntityType = new EntityTypeDef {
 
-      override def parents: Seq[String] = Seq("SlickedRow") //, s"${rawName}Api")
+      override def parents: Seq[String] =
+        Seq("SlickedRow") //, s"${rawName}Api")
 
       override def code = {
         val args = columns.map { c =>
-          c.default.map( v =>
-            s"${c.name}: ${c.exposedType} = $v"
-          ).getOrElse(
-            s"${c.name}: ${c.exposedType}"
-          )
+          c.default
+            .map(v => s"${c.name}: ${c.exposedType} = $v")
+            .getOrElse(
+              s"${c.name}: ${c.exposedType}"
+            )
         }
         val callArgs = columns.map(c => s"${c.name}")
         val types = columns.map(c => c.exposedType)
 
-        val prns = (parents.take(1).map(" extends "+_) ++ parents.drop(1).map(" with "+_)).mkString("")
+        val prns = (parents.take(1).map(" extends " + _) ++ parents
+          .drop(1)
+          .map(" with " + _)).mkString("")
         if (classEnabled) {
           s"""case class $name(${args.mkString(", ")})$prns"""
         } else {
           s"""/** Constructor for $name providing default values if available in the database schema. */
-             |case class $name(${args.map(arg => {s"$arg"}).mkString(", ")})$prns
+             |case class $name(${args
+               .map(arg => { s"$arg" })
+               .mkString(", ")})$prns
              |type ${name}List = ${compoundType(types)}
              |object $name {
              |  def apply(hList: ${name}List): $name = hList match {
-             |    case ${compoundValue(callArgs)} => new $name(${callArgs.mkString(", ")})
+             |    case ${compoundValue(callArgs)} => new $name(${callArgs
+               .mkString(", ")})
              |    case _ => throw new Exception("malformed HList")
              |  }
-             |  def unapply(row: $name) = Some(${compoundValue(callArgs.map(a => s"row.$a"))})
+             |  def unapply(row: $name) = Some(${compoundValue(
+               callArgs.map(a => s"row.$a"))})
              |}
                 """.stripMargin
         }
@@ -71,9 +108,18 @@ class SlickedSourceCodeGenerator(model: Model)
 
     override def PlainSqlMapper = new PlainSqlMapperDef {
       override def code = {
-        val positional = compoundValue(columnsPositional.map(c => if (c.fakeNullable || c.model.nullable) s"<<?[${c.rawType}]" else s"<<[${c.rawType}]"))
-        val dependencies = columns.map(_.exposedType).distinct.zipWithIndex.map{ case (t,i) => s"""e$i: GR[$t]"""}.mkString(", ")
-        val rearranged = compoundValue(desiredColumnOrder.map(i => if(columns.size > 22) s"r($i)" else tuple(i)))
+        val positional = compoundValue(
+          columnsPositional.map(c =>
+            if (c.fakeNullable || c.model.nullable) s"<<?[${c.rawType}]"
+            else s"<<[${c.rawType}]"))
+        val dependencies = columns
+          .map(_.exposedType)
+          .distinct
+          .zipWithIndex
+          .map { case (t, i) => s"""e$i: GR[$t]""" }
+          .mkString(", ")
+        val rearranged = compoundValue(desiredColumnOrder.map(i =>
+          if (columns.size > 22) s"r($i)" else tuple(i)))
         def result(args: String) = s"$factory($args)"
         val body =
           if (autoIncLastAsOption && columns.size > 1) {
@@ -99,20 +145,20 @@ class SlickedSourceCodeGenerator(model: Model)
 
     override def TableClass = new TableClassDef {
 
-
-
       override def parents: Seq[String] = Seq("SlickedTable")
 
       override def star = {
-        val struct = compoundValue(columns.map(c=>if(c.fakeNullable)s"Rep.Some(${c.name})" else s"${c.name}"))
+        val struct = compoundValue(columns.map(c =>
+          if (c.fakeNullable) s"Rep.Some(${c.name})" else s"${c.name}"))
         val rhs = s"$struct <> ($factory, $extractor)"
         s"def * = $rhs"
       }
 
       override def code = {
         val prns = parents.map(" with " + _).mkString("")
-        val args = Seq("\""+model.name.table+"\"")
-        s"""class $name(_tableTag: Tag) extends profile.api.Table[$elementType](_tableTag, ${args.mkString(", ")})$prns {
+        val args = Seq("\"" + model.name.table + "\"")
+        s"""class $name(_tableTag: Tag) extends profile.api.Table[$elementType](_tableTag, ${args
+             .mkString(", ")})$prns {
            |  ${indent(body.map(_.mkString("\n")).mkString("\n\n"))}
            |}
               """.stripMargin
--- src/test/scala/model/Tables.scala
+++ src/test/scala/model/Tables.scala
@@ -1,17 +1,13 @@
 package model
 
-import slicked.SlickMappers
-// AUTO-GENERATED Slick data model
-/** Stand-alone Slick data model for immediate use */
-object Tables extends {
-  val profile = slick.jdbc.H2Profile
-} with Tables
+trait Tables extends slicked.SlickMappers {
 
-/** Slick data model trait for extension, choice of backend or usage in the cake pattern. (Make sure to initialize this late.) */
-trait Tables extends SlickMappers {
+  self: slicked.HasDatabaseProfile =>
+
   val profile: slick.jdbc.JdbcProfile
   import profile.api._
 
+  
   // Custom imports start
   import slicked.SlickedRow
   import slicked.SlickedTable
@@ -22,183 +18,118 @@ trait Tables extends SlickMappers {
   import slick.jdbc.{GetResult => GR}
 
   /** DDL for all tables. Call .create to execute. */
-  lazy val schema
-    : profile.SchemaDescription = CompanyCopy30Table.schema ++ CompanyCopy31Table.schema ++ CompanyTable.schema ++ EventTable.schema
+  lazy val schema: profile.SchemaDescription = CompanyCopy30Table.schema ++ CompanyCopy31Table.schema ++ CompanyTable.schema ++ EventTable.schema
   @deprecated("Use .schema instead of .ddl", "3.0")
   def ddl = schema
 
   /** Entity class storing rows of table CompanyCopy30Table
-    *  @param id Database column ID SqlType(BIGINT), AutoInc, PrimaryKey
-    *  @param name Database column NAME SqlType(VARCHAR), Length(255,true)
-    *  @param address Database column ADDRESS SqlType(VARCHAR) */
-  case class CompanyCopy30(id: Long, name: String, address: Option[String])
-      extends SlickedRow
-
+   *  @param id Database column ID SqlType(BIGINT), AutoInc, PrimaryKey
+   *  @param name Database column NAME SqlType(VARCHAR), Length(255,true)
+   *  @param address Database column ADDRESS SqlType(VARCHAR) */
+  case class CompanyCopy30(id: Long, name: String, address: Option[String]) extends SlickedRow
   /** GetResult implicit for fetching CompanyCopy30 objects using plain SQL queries */
-  implicit def GetResultCompanyCopy30(
-      implicit e0: GR[Long],
-      e1: GR[String],
-      e2: GR[Option[String]]): GR[CompanyCopy30] = GR { prs =>
-    import prs._
+  implicit def GetResultCompanyCopy30(implicit e0: GR[Long], e1: GR[String], e2: GR[Option[String]]): GR[CompanyCopy30] = GR{
+    prs => import prs._
     CompanyCopy30.tupled((<<[Long], <<[String], <<?[String]))
   }
-
+                
   /** Table description of table COMPANY_COPY_3_0. Objects of this class serve as prototypes for rows in queries. */
-  class CompanyCopy30Table(_tableTag: Tag)
-      extends profile.api.Table[CompanyCopy30](_tableTag, "COMPANY_COPY_3_0")
-      with SlickedTable {
+  class CompanyCopy30Table(_tableTag: Tag) extends profile.api.Table[CompanyCopy30](_tableTag, "COMPANY_COPY_3_0") with SlickedTable {
     def * = (id, name, address) <> (CompanyCopy30.tupled, CompanyCopy30.unapply)
-
     /** Maps whole row to an option. Useful for outer joins. */
-    def ? =
-      (Rep.Some(id), Rep.Some(name), address).shaped.<>(
-        { r =>
-          import r._; _1.map(_ => CompanyCopy30.tupled((_1.get, _2.get, _3)))
-        },
-        (_: Any) =>
-          throw new Exception("Inserting into ? projection not supported."))
+    def ? = (Rep.Some(id), Rep.Some(name), address).shaped.<>({r=>import r._; _1.map(_=> CompanyCopy30.tupled((_1.get, _2.get, _3)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
 
     /** Database column ID SqlType(BIGINT), AutoInc, PrimaryKey */
     val id: Rep[Long] = column[Long]("ID", O.AutoInc, O.PrimaryKey)
-
     /** Database column NAME SqlType(VARCHAR), Length(255,true) */
-    val name: Rep[String] =
-      column[String]("NAME", O.Length(255, varying = true))
-
+    val name: Rep[String] = column[String]("NAME", O.Length(255,varying=true))
     /** Database column ADDRESS SqlType(VARCHAR) */
     val address: Rep[Option[String]] = column[Option[String]]("ADDRESS")
   }
-
+                
   /** Collection-like TableQuery object for table CompanyCopy30Table */
-  lazy val CompanyCopy30Table = new TableQuery(
-    tag => new CompanyCopy30Table(tag))
+  lazy val CompanyCopy30Table = new TableQuery(tag => new CompanyCopy30Table(tag))
 
   /** Entity class storing rows of table CompanyCopy31Table
-    *  @param id Database column ID SqlType(BIGINT), AutoInc, PrimaryKey
-    *  @param name Database column NAME SqlType(VARCHAR), Length(255,true)
-    *  @param address Database column ADDRESS SqlType(VARCHAR) */
-  case class CompanyCopy31(id: Long, name: String, address: Option[String])
-      extends SlickedRow
-
+   *  @param id Database column ID SqlType(BIGINT), AutoInc, PrimaryKey
+   *  @param name Database column NAME SqlType(VARCHAR), Length(255,true)
+   *  @param address Database column ADDRESS SqlType(VARCHAR) */
+  case class CompanyCopy31(id: Long, name: String, address: Option[String]) extends SlickedRow
   /** GetResult implicit for fetching CompanyCopy31 objects using plain SQL queries */
-  implicit def GetResultCompanyCopy31(
-      implicit e0: GR[Long],
-      e1: GR[String],
-      e2: GR[Option[String]]): GR[CompanyCopy31] = GR { prs =>
-    import prs._
+  implicit def GetResultCompanyCopy31(implicit e0: GR[Long], e1: GR[String], e2: GR[Option[String]]): GR[CompanyCopy31] = GR{
+    prs => import prs._
     CompanyCopy31.tupled((<<[Long], <<[String], <<?[String]))
   }
-
+                
   /** Table description of table COMPANY_COPY_3_1. Objects of this class serve as prototypes for rows in queries. */
-  class CompanyCopy31Table(_tableTag: Tag)
-      extends profile.api.Table[CompanyCopy31](_tableTag, "COMPANY_COPY_3_1")
-      with SlickedTable {
+  class CompanyCopy31Table(_tableTag: Tag) extends profile.api.Table[CompanyCopy31](_tableTag, "COMPANY_COPY_3_1") with SlickedTable {
     def * = (id, name, address) <> (CompanyCopy31.tupled, CompanyCopy31.unapply)
-
     /** Maps whole row to an option. Useful for outer joins. */
-    def ? =
-      (Rep.Some(id), Rep.Some(name), address).shaped.<>(
-        { r =>
-          import r._; _1.map(_ => CompanyCopy31.tupled((_1.get, _2.get, _3)))
-        },
-        (_: Any) =>
-          throw new Exception("Inserting into ? projection not supported."))
+    def ? = (Rep.Some(id), Rep.Some(name), address).shaped.<>({r=>import r._; _1.map(_=> CompanyCopy31.tupled((_1.get, _2.get, _3)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
 
     /** Database column ID SqlType(BIGINT), AutoInc, PrimaryKey */
     val id: Rep[Long] = column[Long]("ID", O.AutoInc, O.PrimaryKey)
-
     /** Database column NAME SqlType(VARCHAR), Length(255,true) */
-    val name: Rep[String] =
-      column[String]("NAME", O.Length(255, varying = true))
-
+    val name: Rep[String] = column[String]("NAME", O.Length(255,varying=true))
     /** Database column ADDRESS SqlType(VARCHAR) */
     val address: Rep[Option[String]] = column[Option[String]]("ADDRESS")
   }
-
+                
   /** Collection-like TableQuery object for table CompanyCopy31Table */
-  lazy val CompanyCopy31Table = new TableQuery(
-    tag => new CompanyCopy31Table(tag))
+  lazy val CompanyCopy31Table = new TableQuery(tag => new CompanyCopy31Table(tag))
 
   /** Entity class storing rows of table CompanyTable
-    *  @param id Database column ID SqlType(BIGINT), AutoInc, PrimaryKey
-    *  @param name Database column NAME SqlType(VARCHAR), Length(255,true)
-    *  @param address Database column ADDRESS SqlType(VARCHAR) */
-  case class Company(id: Long, name: String, address: Option[String])
-      extends SlickedRow
-
+   *  @param id Database column ID SqlType(BIGINT), AutoInc, PrimaryKey
+   *  @param name Database column NAME SqlType(VARCHAR), Length(255,true)
+   *  @param address Database column ADDRESS SqlType(VARCHAR) */
+  case class Company(id: Long, name: String, address: Option[String]) extends SlickedRow
   /** GetResult implicit for fetching Company objects using plain SQL queries */
-  implicit def GetResultCompany(implicit e0: GR[Long],
-                                e1: GR[String],
-                                e2: GR[Option[String]]): GR[Company] = GR {
-    prs =>
-      import prs._
-      Company.tupled((<<[Long], <<[String], <<?[String]))
+  implicit def GetResultCompany(implicit e0: GR[Long], e1: GR[String], e2: GR[Option[String]]): GR[Company] = GR{
+    prs => import prs._
+    Company.tupled((<<[Long], <<[String], <<?[String]))
   }
-
+                
   /** Table description of table COMPANY. Objects of this class serve as prototypes for rows in queries. */
-  class CompanyTable(_tableTag: Tag)
-      extends profile.api.Table[Company](_tableTag, "COMPANY")
-      with SlickedTable {
+  class CompanyTable(_tableTag: Tag) extends profile.api.Table[Company](_tableTag, "COMPANY") with SlickedTable {
     def * = (id, name, address) <> (Company.tupled, Company.unapply)
-
     /** Maps whole row to an option. Useful for outer joins. */
-    def ? =
-      (Rep.Some(id), Rep.Some(name), address).shaped.<>(
-        { r =>
-          import r._; _1.map(_ => Company.tupled((_1.get, _2.get, _3)))
-        },
-        (_: Any) =>
-          throw new Exception("Inserting into ? projection not supported."))
+    def ? = (Rep.Some(id), Rep.Some(name), address).shaped.<>({r=>import r._; _1.map(_=> Company.tupled((_1.get, _2.get, _3)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
 
     /** Database column ID SqlType(BIGINT), AutoInc, PrimaryKey */
     val id: Rep[Long] = column[Long]("ID", O.AutoInc, O.PrimaryKey)
-
     /** Database column NAME SqlType(VARCHAR), Length(255,true) */
-    val name: Rep[String] =
-      column[String]("NAME", O.Length(255, varying = true))
-
+    val name: Rep[String] = column[String]("NAME", O.Length(255,varying=true))
     /** Database column ADDRESS SqlType(VARCHAR) */
     val address: Rep[Option[String]] = column[Option[String]]("ADDRESS")
   }
-
+                
   /** Collection-like TableQuery object for table CompanyTable */
   lazy val CompanyTable = new TableQuery(tag => new CompanyTable(tag))
 
   /** Entity class storing rows of table EventTable
-    *  @param timestamp Database column TIMESTAMP SqlType(TIMESTAMP)
-    *  @param name Database column NAME SqlType(VARCHAR), Length(255,true)
-    *  @param desc Database column DESC SqlType(VARCHAR), Length(1024,true) */
-  case class Event(timestamp: Option[org.joda.time.DateTime],
-                   name: Option[String],
-                   desc: Option[String])
-      extends SlickedRow
-
+   *  @param timestamp Database column TIMESTAMP SqlType(TIMESTAMP)
+   *  @param name Database column NAME SqlType(VARCHAR), Length(255,true)
+   *  @param desc Database column DESC SqlType(VARCHAR), Length(1024,true) */
+  case class Event(timestamp: Option[org.joda.time.DateTime], name: Option[String], desc: Option[String]) extends SlickedRow
   /** GetResult implicit for fetching Event objects using plain SQL queries */
-  implicit def GetResultEvent(implicit e0: GR[Option[org.joda.time.DateTime]],
-                              e1: GR[Option[String]]): GR[Event] = GR { prs =>
-    import prs._
+  implicit def GetResultEvent(implicit e0: GR[Option[org.joda.time.DateTime]], e1: GR[Option[String]]): GR[Event] = GR{
+    prs => import prs._
     Event.tupled((<<?[org.joda.time.DateTime], <<?[String], <<?[String]))
   }
-
+                
   /** Table description of table EVENT. Objects of this class serve as prototypes for rows in queries. */
-  class EventTable(_tableTag: Tag)
-      extends profile.api.Table[Event](_tableTag, "EVENT")
-      with SlickedTable {
+  class EventTable(_tableTag: Tag) extends profile.api.Table[Event](_tableTag, "EVENT") with SlickedTable {
     def * = (timestamp, name, desc) <> (Event.tupled, Event.unapply)
 
     /** Database column TIMESTAMP SqlType(TIMESTAMP) */
-    val timestamp: Rep[Option[org.joda.time.DateTime]] =
-      column[Option[org.joda.time.DateTime]]("TIMESTAMP")
-
+    val timestamp: Rep[Option[org.joda.time.DateTime]] = column[Option[org.joda.time.DateTime]]("TIMESTAMP")
     /** Database column NAME SqlType(VARCHAR), Length(255,true) */
-    val name: Rep[Option[String]] =
-      column[Option[String]]("NAME", O.Length(255, varying = true))
-
+    val name: Rep[Option[String]] = column[Option[String]]("NAME", O.Length(255,varying=true))
     /** Database column DESC SqlType(VARCHAR), Length(1024,true) */
-    val desc: Rep[Option[String]] =
-      column[Option[String]]("DESC", O.Length(1024, varying = true))
+    val desc: Rep[Option[String]] = column[Option[String]]("DESC", O.Length(1024,varying=true))
   }
-
+                
   /** Collection-like TableQuery object for table EventTable */
   lazy val EventTable = new TableQuery(tag => new EventTable(tag))
 }
+      
